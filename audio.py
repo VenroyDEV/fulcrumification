d8128b871993d3df9ce1e08d905b5207b3778e60
Venroy <lschwan4@gmail.com>
2023-07-09 22:02:28 +0200

    quick safe, no clue how to fix this permission issue
    When I started learning python, I already had this problem with creating anything such as a .txt, hmm.

--- audio.py
+++ audio.py
@@ -13,8 +13,8 @@ mp3_file = AudioSegment.from_file(f"{(select_file_tk.select_file())}", format="m
 print("select a file to upload : ")
 
 # Load the audio clips
-clip1 = AudioSegment.from_file(r"D:\github\fulrcumifiaction\fulcrumification\mp3\codeword.mp3", format="mp3")
-clip2 = AudioSegment.from_file(r"D:\github\fulrcumifiaction\fulcrumification\mp3\codeword.mp3", format="mp3")
+clip1 = AudioSegment.from_file(r"D:\\github\\fulrcumifiaction\\fulcrumification\\mp3\\codeword.mp3", format="mp3")
+clip2 = AudioSegment.from_file(r"D:\\github\\fulrcumifiaction\fulcrumification\\mp3\\codeword.mp3", format="mp3")
 
 # Concatenate the audio clips
 concatenated_clips = clip1.append(clip2, crossfade=0)
@@ -23,9 +23,14 @@ concatenated_clips = clip1.append(clip2, crossfade=0)
 final_audio = mp3_file.overlay(concatenated_clips)
 
 # Export the final audio file
-if not os.path.exists("exportstuff"): 
-    os.mkdir("exportstuff")
-    final_audio.export("exportstuff", format="mp3")
- 
+if not os.path.exists("D:\\github\\fulrcumifiaction\\fulcrumification\\exportstuff"): 
+    os.mkdir("D:\\github\\fulrcumifiaction\\fulcrumification\\exportstuff")
+    path = r"D:\\github\\fulrcumifiaction\\fulcrumification\\exportstuff"
+    assert os.path.isdir(path), "path doesnt exist"
 else:
-    final_audio.export("exportstuff", format="mp3")
+     final_audio.export("exportstuff", format="mp3")
+     print("technically everything went fine")
+     with open("test", "w") as f:
+        f.write("test")
+        print("successful")
+        pass
