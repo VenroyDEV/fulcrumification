0fc6f96facbff0a5489c853c1e22a99e4bb20cff
Venroy <lschwan4@gmail.com>
2023-07-09 16:25:29 +0200

    fixxed the formatting
    whoops

--- main.py
+++ main.py
@@ -2,19 +2,9 @@ from select_file import *
 
 
 if __name__ == "__main__":
-
-
    millisecond_duration = get_file_info(select_file())
    seconds_duration =  millisecond_duration /1000
    seconds = seconds_duration % 60
    minutes_duration = seconds_duration // 60
 
-   print(f"{int(minutes_duration}:{seconds}")
-         
-
-def readable_format():
-   seconds_duration =  millisecond_duration /1000
-   seconds = seconds_duration % 60
-   minutes_duration = seconds_duration // 60
-
-   print(f"{int(minutes_duration)}:{seconds}")
+   print(f"{int(minutes_duration)}:{int(seconds)}")
